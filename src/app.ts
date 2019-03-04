e0fb64a0c24ee334da44dddea62a5a594d961fae
booellean <booelle.commissions@gmail.com>
2019-03-04 15:40:12 -0800

    feat: modified code to run multiple routes, included body parser for blog post use later

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,45 @@
-import * as express from 'express'
+//express imports
+import * as express from 'express';
+import {Request, Response} from "express";
+//body Parser
+import * as bodyParser from "body-parser";
+//Routes imported
+import { HomeRoutes } from './routes/homeRoutes';
+import { BlogRoutes } from './routes/blogRoutes';
+
 
 class App {
-  public express
+  public app: express.Application;
+  public homeRoute: HomeRoutes = new HomeRoutes();
+  public blogRoute: BlogRoutes = new BlogRoutes();
 
   constructor () {
-    this.express = express();
-    this.mountRoutes();
+    this.app = express();
+    this.config();
+    this.homeRoute.homeRoutes(this.app);
+    this.blogRoute.blogRoutes(this.app);
+
+    // this.mountRoutes();
   }
 
-  private mountRoutes (): void {
-    const router = express.Router();
-    this.express.use( express.static(__dirname + '/public'));
+  // private mountRoutes (): void {
+  //   const router = express.Router();
+  //   this.app.use( express.static(__dirname + '/public'));
 
-    router.get('/', (req, res) => {
-      // res.json({
-      //   message: 'Hello World!'
-      // });
-      res.sendFile('index.html', { root: __dirname + '/public' } );
-    });
-    this.express.use('/', router);
+  //   router.get('/', (req: Request, res: Response) => {
+  //     // res.json({
+  //     //   message: 'Hello World!'
+  //     // });
+  //     res.sendFile('index.html', { root: __dirname + '/public' } );
+  //   });
+  //   this.app.use('/', router);
+  // }
+
+  private config(): void{
+    this.app.use(express.static(__dirname + '/public'));
+    this.app.use(bodyParser.json());
+    this.app.use(bodyParser.urlencoded({ extended: false }));
   }
 }
 
-export default new App().express;
+export default new App().app;
