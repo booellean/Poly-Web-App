1f781a0c55024b6cb903f07685cbc6d4929bacf1
booellean <booelle.commissions@gmail.com>
2019-03-23 16:03:48 -0700

    feat: Restructuring App for pre-emptive customizable system
    All theme elements (public files and theme templates) in new folder called themes/base-theme
    Updated all file paths approrpiately to reflect this change.  Will eventually create a master config to work with active and inactive themes

--- src/app.ts
+++ src/app.ts
@@ -45,9 +45,9 @@ class App {
   // }
 
   private config(): void{
-    this.app.use(express.static(__dirname + '/public'));
+    this.app.use(express.static(__dirname + '/themes/base-theme/public'));
     this.app.set('view engine', 'ejs');
-    this.app.set('views', __dirname + '/views');
+    this.app.set('views', __dirname + '/themes/base-theme/views');
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: false }));
   }
